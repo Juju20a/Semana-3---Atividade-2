c6cb87dcae4717adef39eb5bc488a40a88a89641
Juju20a <juliene.barbosa@academico.ifpb.edu.br>
2024-11-13 15:05:45 -0300

    algoritimo2.js

--- Semana 3- atividade2/src/algoritimo2.js
+++ Semana 3- atividade2/src/algoritimo2.js
@@ -6,9 +6,9 @@ const letras = [
     
     let resultado = letras.flat().reduce((objeto, numero) => { //Cria uma nova matriz com todos os elementos da submatriz concatenados recursivamente até a profundidade especificada. 
         if (objeto[numero]) {
-       objeto[numero] += 1;
+       objeto[numero] += 1; //Se a chave já existe, incrementa o valor (contagem)
      } else {
-       objeto[numero] = 1;
+       objeto[numero] = 1;// Se não existe incrementa apartir de 1
      }
      return objeto; //Retorna objeto
     }, {});
